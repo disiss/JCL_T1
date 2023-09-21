00ad5f9a5b25091104d99c00a657020319f144eb
disiss <66673593+disiss@users.noreply.github.com>
2023-09-21 22:08:58 +0300

    Add files via upload

--- JCL.sh
+++ JCL.sh
@@ -2,13 +2,21 @@
 
 #only for 20.04 ubuntu
 
+GREEN=$'\e[0;32m'
+RED=$'\e[0;31m'
+NC=$'\e[0m'
+
 apt update
-apt install -y dante-server libpam-pwdfile openssl curl git
+apt install -y dante-server libpam-pwdfile openssl curl git apt install python3-pip
+
+echo "Activating ufw"
+ufw enable
+echo "${GREEN}Ufw in activated!${NC}"
 
 ufw allow 1124/tcp
 
-echo "Opened 1 server port:"
-echo "1124"
+echo "${GREEN}Opened 1 server port:${NC}"
+echo "${RED}1124${NC}"
 
 
 ufw allow 1080/tcp
@@ -16,20 +24,22 @@ ufw allow 1101/tcp
 ufw allow 1122/tcp
 ufw allow 1212/tcp
 
-echo "Opened 4 socks ports:"
-echo "1080"
+echo "${GREEN}Opened 4 socks ports:${NC}"
+echo "${RED}1080"
 echo "1101"
 echo "1122"
-echo "1212"
+echo "1212${NC}"
 
-echo "Installing JCL downloader..."
+echo "${RED}Installing JCL...${NC}"
 cd
 git clone https://github.com/disiss/JCL_T1
+wait
 cd JCL_T1
 
-echo "Installing requirements..."
+echo "${RED}Installing requirements...${NC}"
 pip3 install -r requirements.txt
-echo "Requirements in installed!"
+echo "${GREEN}Requirements in installed!"
+echo "JCL is succ installed!${NC}"
 
 # echo "Installing requirements..."
 # python3 generate_proxies.py
